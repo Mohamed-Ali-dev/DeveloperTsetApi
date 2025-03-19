b4ba505263b284691337a7160b204a7e799dccf2
mali <moohamedali571@gmail.com>
2025-03-19 09:25:57 +0200

    Connect to database

---
+++ DeveloperTestApi/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using DeveloperTestApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeveloperTestApi.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.ToTable("Accounts");
+                entity.HasKey(e => e.ACC_number);
+                entity.Property(e => e.ACC_number)
+                .HasColumnName("ACC_Number")
+                .HasMaxLength(10)
+                .IsRequired();
+
+                entity.Property(e => e.ACC_Parent)
+                .HasColumnName("ACC_Parent")
+                .HasMaxLength(10);
+
+                entity.Property(e => e.Balance)
+                .HasColumnName("Balance")
+                .HasColumnType("decimal(20,9");
+
+                entity.HasOne(e => e.ParentAccount)
+                .WithMany(e => e.ChildAccounts)
+                .HasForeignKey(e => e.ACC_Parent)
+                .HasConstraintName("FK_Account_Account");
+
+            });
+        }
+    }
+}
---
+++ DeveloperTestApi/Model/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeveloperTestApi.Model
+{
+    public class Account
+    {
+        public string ACC_number { get; set; }
+        public decimal? Balance { get; set; }
+        public string? ACC_Parent { get; set; }
+        public Account ParentAccount { get; set; }
+        public ICollection<Account> ChildAccounts { get; set; } = new List<Account>();
+    }
+}
